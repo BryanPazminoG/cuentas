0097888f45b95acdeb67bb0e96a5a47ab3d31fae
Sebastian Jaramillo <hsjj2000@gmail.com>
2024-02-13 19:42:31 -0500

    Cross Origin

--- src/main/java/com/banquito/core/baking/cuenta/controller/TarjetaController.java
+++ src/main/java/com/banquito/core/baking/cuenta/controller/TarjetaController.java
@@ -1,6 +1,7 @@
 package com.banquito.core.baking.cuenta.controller;
 
 import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,16 +11,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.banquito.core.baking.cuenta.dto.TarjetaDTO;
 import com.banquito.core.baking.cuenta.service.TarjetaService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-//@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "", allowedHeaders = "", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
+@CrossOrigin(origins = {"http://localhost:4200", "http://34.173.161.134:4201", "http://34.176.205.203:4202", 
+                        "http://34.176.102.118:4203", "http://34.176.137.180:4204"})
+//@CrossOrigin(origins = "", allowedHeaders = "", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
 @RestController
 @RequestMapping("/api/v1/tarjetas")
 public class TarjetaController {
